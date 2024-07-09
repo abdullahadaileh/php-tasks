20db9ff0536c7cc90704841bec8f01ae7a632213
ABDULLAH ADAILEH <169062688+abdullahadaileh@users.noreply.github.com>
2024-07-09 06:09:20 +0300

    Update index.php

--- php tasks/week1-b/index.php
+++ php tasks/week1-b/index.php
@@ -2,23 +2,16 @@
 // START task1
 
 // $fruits = array('Apple', 'Banana', 'Cherry', 'Date', 'Elderberry');
+// $fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+
+// array_push($fruits,"some");
+
+// array_pop($fruits);
 
 
 // for ($i=0; $i <count($fruits) ; $i++) { 
 //     echo "<h3>{$fruits[$i]}</h3>";
 // }
-
-
-
-// array_push($fruits,"Papaya");
-// print_r($fruits);
-
-
-
-// array_splice($fruits,5);
-// print_r($fruits);
-
-
 
 // if (in_array("Apple",$fruits))
 // {
@@ -42,22 +35,20 @@
 //     "Saleh" => 25
 // ];
 
-// $users ["Ala"]=27; // task2-c
 
-// unset($users["Abdullrahman"]); // task2-d
+// $users ["Ala"]=23;
 
+// unset($users["Ala"]);
+
+// if (array_key_exists("Ala",$users)) {
+//     echo "we found him";
+// }else{
+//     echo "we didnt foun him";
+// }
 
 // foreach($users as $name => $age){
 //     echo "<h3>$name,$age</h3>";
 // }
-
-
-// if (array_key_exists("Abdllah",$users)) {
-//     echo "<h3>we found him</h3>";
-// }else{
-//     echo "<h3>we didnt found him</h3>";
-// }
-
 
 // END task2
 
@@ -74,17 +65,19 @@ $students =
 
 ];
 
-$new_student = ["Name" => "Mohammad", "Age" => 21, "grade" => 89];
+
+$new_student =["Name"=>"Hamed","Age"=>17,"grade"=>80];
 $students[] = $new_student;
 
-$student_Name = "Abdullah";
-$new_grade = 100;
+$student_name = "Abdullah";
+$new_grade = 99;
 
 foreach($students as &$student){
-    if ($student["Name"] === $student_Name) {
-        $student["grade"] = $new_grade;
+    if ($student["Name"]==$student_name) {
+        $student["grade"]=$new_grade;
     }
 }
+
 
 foreach ($students as $student){
     echo "<h3>Name:{$student['Name']},Age:{$student['Age']},grade:{$student['grade']}</h3>";
@@ -92,6 +85,8 @@ foreach ($students as $student){
 
 }
 
+
+// END task3
 
 
 
